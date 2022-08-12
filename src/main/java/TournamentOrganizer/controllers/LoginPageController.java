ac276def9aab4e319aa6ece1171796203f710174
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-08-11 20:08:26 -0500

    finally can see the template in the view!

--- src/main/java/TournamentOrganizer/controllers/LoginPageController.java
+++ src/main/java/TournamentOrganizer/controllers/LoginPageController.java
@@ -2,23 +2,16 @@ package TournamentOrganizer.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginPageController {
 
-    @GetMapping("/login")
-    @ResponseBody
-    public String loginPage(Model model) {
-        return "I know that you work now!";
+
+    @GetMapping("login")
+    public String loginPage() {
+        return "login";
     }
 
-//    @GetMapping("/login") //this obviously needs flushing out
-//    public String username(Model model) {
-//        return "username";
-//
-//    }
 
 }
