a59f447962731695b6bed641492a98f6d40bb2bd
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-08-17 18:22:57 -0500

    flushing out the login page!!

---
+++ src/main/java/TournamentOrganizer/controllers/SignUpPageController.java
@@ -0,0 +1,15 @@
+package TournamentOrganizer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class SignUpPageController {
+
+    @GetMapping("signup")
+        public String signupPage() {
+            return "signupPage";
+        }
+    }
+
